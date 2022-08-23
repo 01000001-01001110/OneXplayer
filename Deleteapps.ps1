112a6809362ac99750d6a04b67bbba09655b9ab1
01000001-01001110 <48245017+01000001-01001110@users.noreply.github.com>
2022-08-23 09:24:28 -0400

    Update Deleteapps.ps1
    Added 26 more Windows Store apps that bloat out the system. 
    
    Keeping search and Cortana and other game functions to not break the device, but just debloat.

--- Deleteapps.ps1
+++ Deleteapps.ps1
@@ -39,3 +39,82 @@ Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
 Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
 Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
 Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+
+#More Applications
+#Cloud Experience
+Get-AppxPackage -Name "Microsoft.Windows.CloudExperienceHost" -AllUsers | Remove-AppxPackage
+
+#bio Enrollment
+Get-AppxPackage -Name "Microsoft.BioEnrollment" -AllUsers | Remove-AppxPackage 
+
+#AAD Broker
+Get-AppxPackage -Name "Microsoft.AAD.BrokerPlugin" -AllUsers | Remove-AppxPackage 
+
+#CBSPreview
+Get-AppxPackage -Name "Windows.CBSPreview" -AllUsers | Remove-AppxPackage
+
+#Parental Controls
+Get-AppxPackage -Name "Microsoft.Windows.ParentalControls" -AllUsers | Remove-AppxPackage
+
+#Print Dialogue
+Get-AppxPackage -Name "Windows.PrintDialog" -AllUsers | Remove-AppxPackage
+
+#Onedrive
+Get-AppxPackage -Name "Microsoft.OneDriveSync" -AllUsers | Remove-AppxPackage
+
+#Teams
+Get-AppxPackage -Name "MicrosoftTeams" -AllUsers | Remove-AppxPackage
+
+#Sticky Notes
+Get-AppxPackage -Name "Microsoft.MicrosoftStickyNotes" -AllUsers | Remove-AppxPackage
+
+#Paint
+Get-AppxPackage -Name "Microsoft.MicrosoftStickyNotes" -AllUsers | Remove-AppxPackage
+
+#Screen Sketch
+Get-AppxPackage -Name "Microsoft.ScreenSketch" -AllUsers | Remove-AppxPackage
+
+#Paint
+Get-AppxPackage -Name "Microsoft.Paint" -AllUsers | Remove-AppxPackage
+
+#Maps
+Get-AppxPackage "Microsoft.WindowsMaps" | Remove-AppxPackage
+
+#People
+Get-AppxPackage "Microsoft.People" | Remove-AppxPackage
+
+#Phone Link
+Get-AppxPackage "Microsoft.YourPhone" -AllUsers | Remove-AppxPackage
+
+#Quick Assist
+Remove-WindowsCapability -Online -Name $(Get-WindowsCapability -Online -Name *quickassist*)
+
+#Tips
+Get-appxpackage "Microsoft.Getstarted" -AllUsers | Remove-AppxPackage -allusers
+
+#Get Help
+Get-AppxPackage -Name "Microsoft.GetHelp" -AllUsers | Remove-AppxPackage
+
+#to-Do's
+Get-AppxPackage -Name "Microsoft.todos" | Remove-AppxPackage
+
+#Hulu
+Get-AppxPackage -Name "HULULLC.HULUPLUS" -AllUsers | Remove-AppxPackage
+
+#Netflix
+Get-AppxPackage -Name "4DF9E0F8.Netflix" -AllUsers | Remove-AppxPackage
+
+#Spotify
+Get-AppxPackage -Name "SpotifyAB.SpotifyMusic" -AllUsers | Remove-AppxPackage
+
+#Weather
+Get-AppxPackage -Name "Microsoft.BingWeather" -AllUsers | Remove-AppxPackage
+
+#Microsoft Edge
+Get-AppxPackage -Name "Microsoft.MicrosoftEdge.Stable" -AllUsers | Remove-AppxPackage 
+
+#Windows Client Web Experience
+Get-AppxPackage -Name "MicrosoftWindows.Client.WebExperience" -AllUsers | Remove-AppxPackage
+
+#Messaging
+Get-AppxPackage -Name "Microsoft.Messaging" -AllUsers | Remove-AppxPackage
